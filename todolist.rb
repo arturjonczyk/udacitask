da08defa5e1e997c581a816062567aea1d953157
Artur Jonczyk <artur.jonczyk@gmail.com>
2016-04-23 16:06:11 +0200

    Added basic functionality. - add task - remove task - change status - print tasks - change title of the todo list

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,60 @@
 class TodoList
+    attr_reader :title, :items
+
     # methods and stuff go here
+    def initialize(list_title)
+        @title = list_title
+        @items = []
+    end
+
+    # add task to the list
+    def add_item(new_item)
+        item = Item.new(new_item)
+        @items << item
+    end
+
+    # printing well formated title of the todo list
+    def print_list_title
+        puts '-' * @title.length
+        puts @title
+        puts '-' * @title.length
+    end
+
+    # printing title of the list and every task
+    def print_items
+        # printing title of the ToDo list
+        print_list_title
+
+        # printing each task like so: '1 - "Name of the task"  Completed: false'
+        @items.each_with_index do |item, index|
+            puts "#{index + 1} - #{('"' + item.description + '"').ljust(15, " ")} Completed: #{item.completed_status}"
+        end
+    end
+
+    # removing specific task
+    def delete_item(index)
+        @items.delete_at(index - 1)
+    end
+
+    # changing status on specific task
+    def update_status(index, status)
+        new_status = status ? true : false
+        @items[index - 1].completed_status = new_status
+    end
+
+    # changing the title of the todo list
+    def update_title(new_title)
+        @title = new_title
+    end
 end
 
 class Item
+    attr_accessor :completed_status
+    attr_reader :description
+
     # methods and stuff go here
+    def initialize(description)
+        @description = description
+        @completed_status = false
+    end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,37 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+list = TodoList.new("Things to learn")
 
 # Add four new items
+list.add_item("Variables")
+list.add_item("Arrays")
+list.add_item("Hashes")
+list.add_item("Classes")
 
 # Print the list
+list.print_items
 
 # Delete the first item
+list.delete_item(1)
 
 # Print the list
+list.print_items
 
 # Delete the second item
+list.delete_item(2)
 
 # Print the list
+list.print_items
 
 # Update the completion status of the first item to complete
+list.update_status(1, true)
 
 # Print the list
+list.print_items
 
 # Update the title of the list
+list.update_title("Happy Learning List:")
 
 # Print the list
+list.print_items
